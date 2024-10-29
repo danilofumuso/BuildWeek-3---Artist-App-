de9f403f25789d8cdf61d43cb99f16e7bfbdf808
danilofumuso <danilofumuso@gmail.com>
2024-10-29 18:09:21 +0100

    drama

--- artistApp/src/app/shared/post/post.component.ts
+++ artistApp/src/app/shared/post/post.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { iPost } from '../../interfaces/i-post';
+import { iComment } from '../../interfaces/i-comment';
+import { CommentsService } from '../../services/comments.service';
 
 @Component({
   selector: 'app-post',
@@ -8,6 +10,18 @@ import { iPost } from '../../interfaces/i-post';
 })
 export class PostComponent {
   @Input() post!: iPost;
+  @Input() comments: iComment[] = [];
+
+  visible: boolean = true;
 
   isCollapsed: boolean = true;
+
+  constructor(private commentsSvc: CommentsService) {}
+
+  showComments(postId: number) {
+    this.commentsSvc.getCommentsOfPost(postId).subscribe((comments) => {
+      this.comments = comments;
+      this.visible = !this.visible;
+    });
+  }
 }
