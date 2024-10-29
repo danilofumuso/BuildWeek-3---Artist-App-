f8942a4b061508d24ee8122cf3d1ae6886343460
danilofumuso <danilofumuso@gmail.com>
2024-10-29 09:26:25 +0100

    post component

--- artistApp/src/app/shared/post/post.component.ts
+++ artistApp/src/app/shared/post/post.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { iPost } from '../../interfaces/i-post';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrl: './post.component.scss'
+  styleUrl: './post.component.scss',
 })
 export class PostComponent {
+  @Input() post!: iPost;
 
+  isCollapsed: boolean = true;
 }
